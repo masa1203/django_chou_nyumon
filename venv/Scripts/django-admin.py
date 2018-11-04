c225394ee280ef2cbd6188b7413f6e86a6f015e5
masa1203 <msys.sawada@gmail.com>
2018-11-04 23:17:40 +0900

    start project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\masayoshi\documents\03．お勉強\04．python\pythondjango超入門\django_start\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
