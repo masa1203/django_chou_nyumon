e130e62c8a6d45c8f2100e0bce79c6f8784482de
masa1203 <msys.sawada@gmail.com>
2018-11-05 00:11:21 +0900

    tried the query_parameter

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello Django!")
+def index(request, id, nickname):
+    result = 'your id:"' + str(id) + ', name:"'\
+         + nickname + '".'
+    return HttpResponse(result)
