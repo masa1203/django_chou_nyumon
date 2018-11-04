3c68e40dcaf72b40f67c0c3688d462ae38ca35b6
masa1203 <msys.sawada@gmail.com>
2018-11-05 01:11:15 +0900

    add the stylesheet and next url

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request, id, nickname):
-    result = 'your id:"' + str(id) + ', name:"'\
-         + nickname + '".'
-    return HttpResponse(result)
+def index(request):
+    params = {
+        'title':'Hello/index',
+        'msg':'this is sample page.',
+        'goto':'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'this is next page.',
+        'goto':'index',
+    }
+    return render(request, 'hello/index.html', params)
