0479e39aa7664bb299df514ee4f5204233bf2f2b
masa1203 <msys.sawada@gmail.com>
2018-11-10 16:32:16 +0900

    forms.py checkbox test

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 
 # Create your views here.
 
-def index(request):
-    params = {
-        'title':'Hello',
-        'message':'your data:',
-        'form': HelloForm()
-    }
-    if(request.method == 'POST'):
-        params['message'] = '名前:' + request.POST['name'] + \
-            '<br>メール:' + request.POST['mail'] + \
-            '<br>年齢:' + request.POST['age']
-        params['form'] = HelloForm(request.POST)
+class HelloView(TemplateView):
 
-    return render(request, 'hello/index.html', params)
-
-def form(request):
-    msg = request.POST['msg']
-    params = {
-        'title':'Hello/Next',
-        'msg':'Hi,' + msg + '.',
-    }
-    return render(request, 'hello/index.html', params)
+    def __init__(self):
+        self.params = {
+            'title':'Hello',
+            'form':HelloForm(),
+            'result':None,
+        }
+    
+    def get(self,request):
+        return render(request, 'hello/index.html', self.params)
+    
+    def post(self, request):
+        chk = request.POST['check']
+        self.params['result'] = 'your selected :"' + chk + '".'
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
+        
