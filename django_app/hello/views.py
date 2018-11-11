40da64132560c2030c7f11bc8c58834ee75099bf
masa1203 <msys.sawada@gmail.com>
2018-11-11 19:42:17 +0900

    add the create.html

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.shortcuts import redirect
 from .forms import HelloForm
 from .models import Friend
 
 # Create your views here.
 
 def index(request):
-    data = Friend.objects.all().values()
+    data = Friend.objects.all()
     params = {
         'title':'Hello',
         'data':data,
     }
     return render(request, 'hello/index.html', params)
+
+#create model
+def create(request):
+    params = {
+        'title':'Hello',
+        'form':HelloForm(),
+    }
+
+    if(request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender = 'gender' in request.POST
+        age = int(request.POST['age'])
+        birth = request.POST['birthday']
+        friend = Friend(name=name, mail=mail, gender=gender, \
+                age=age, birthday=birth)
+        friend.save()
+        return redirect(to='/hello')
+    return render(request, 'hello/create.html', params)
